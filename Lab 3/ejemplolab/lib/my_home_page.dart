7bf8ec2a3a858cf61a0d01c9d64a40440c7dcc95
sonic7san3 <msaucedoh2001@gmail.com>
2024-04-25 18:45:36 -0400

    Punto 4 mitad
    Botones cambian el texto y proceso de hacer la carta

--- Lab 3/ejemplolab/lib/my_home_page.dart
+++ Lab 3/ejemplolab/lib/my_home_page.dart
@@ -3,17 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -21,15 +11,20 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   String iconobased= "assets/icons/sonic.svg";
+  String resultado = ""; 
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+     
       _counter++;
+       if(_counter >= 15)
+    {
+      resultado = "Ganaste";
+    }
+    else  if(_counter == 10)
+    {
+      resultado = "Perdiste";
+    }
     });
   }
 
@@ -37,6 +32,14 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
 
       _counter--;
+      if(_counter >= 15)
+    {
+      resultado = "Ganaste";
+    }
+    else  if(_counter == 14)
+    {
+      resultado = "Perdiste";
+    }
     });
   }
 
@@ -44,44 +47,30 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
 
       _counter = 0;
+      resultado = "Empecemos de nuevo";
     });
+
+    void _nombreResultado()
+    {
+       
+   
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+   
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+       
         backgroundColor: Color.fromARGB(255, 2, 36, 174),
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+       
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -92,6 +81,8 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+             Text(resultado, style: TextStyle(fontFamily: "SonicFont")), 
+            
           ],
         ),
       ),
@@ -99,7 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Aumentar',
         child: const Icon(Icons.plus_one),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
       persistentFooterAlignment: AlignmentDirectional.bottomCenter,
       persistentFooterButtons: [
         Row(
@@ -126,7 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
         )
 
      ],
-    
+  
         );
 
   }
